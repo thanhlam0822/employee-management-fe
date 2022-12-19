ddc0ffc6c309ae3d7299da284cb3ed3f5eae55aa
thanhlam0822 <benzamano123@gmail.com>
2022-12-19 13:44:01 +0700

    add form

--- src/components/AddDepartment.vue
+++ src/components/AddDepartment.vue
@@ -6,7 +6,27 @@
       width="30%"
       :before-close="handleClose"
   >
-    <span>This is a message</span>
+    <div class="form-add">
+      <span>Name:</span>
+     <div class="input">
+       <el-col :span="20">
+         <el-input v-model="input" placeholder="Name Department" />
+       </el-col>
+     </div>
+      <span>Responbility</span>
+      <div class="input">
+        <el-col :span="20">
+          <el-input
+              v-model="textarea"
+
+              type="textarea"
+              placeholder="Please input"
+          />
+        </el-col>
+      </div>
+    </div>
+
+
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">Cancel</el-button>
@@ -26,5 +46,11 @@ const handleClose = () => {
 }
 </script>
 <style scoped>
+.form-add {
 
+  align-items: center;
+}
+.input {
+  margin: 10px;
+}
 </style>
